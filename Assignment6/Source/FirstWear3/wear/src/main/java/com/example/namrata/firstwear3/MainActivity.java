ac58f8df90597c0b50b2b78273f9c7252ba8e988
NamrataDutta <nd8gv@mail.umkc.edu>
2017-03-01 22:27:34 -0600

    Lab6

---
+++ Assignment6/Source/FirstWear3/wear/src/main/java/com/example/namrata/firstwear3/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.namrata.firstwear3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+    public void onClickLoc(View v) {
+
+        if (v.getId() == R.id.butTouch) {
+
+            Intent i = new Intent(MainActivity.this, MapsActivity.class);
+            startActivity(i);
+        }
+
+    }
+}
